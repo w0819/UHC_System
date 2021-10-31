364c308c0a7ac7f4200945e98879edf5fd180295
w0819 <hhha3730@gmail.com>
2021-10-31 11:46:46 +0900

    백롤

--- src/main/kotlin/com/github/w0819/main/Main.kt
+++ src/main/kotlin/com/github/w0819/main/Main.kt
@@ -1,19 +1,1 @@
-package com.github.w0819.main
 
-import com.github.w0819.UHCRecipe
-import com.github.w0819.event.Event
-import org.bukkit.plugin.java.JavaPlugin
-
-
-class Main : JavaPlugin() {
-    companion object {
-        @JvmStatic
-        val recipeList = ArrayList<UHCRecipe>()
-    }
-
-    override fun onEnable() {
-        server.pluginManager.registerEvents(Event(this), this)
-        server.logger.info("Recipe is onEnable")
-
-    }
-}
