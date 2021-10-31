2e5b41d2aba143ac3704ccb63e03c6ec4e5c97c2
w0819 <hhha3730@gmail.com>
2021-10-31 11:50:00 +0900

    ㅎ

--- src/main/kotlin/com/github/w0819/main/Main.kt
+++ src/main/kotlin/com/github/w0819/main/Main.kt
@@ -1,1 +1,55 @@
+import com.github.w0819.UHCRecipe
+import com.github.w0819.event.Event
+import com.github.w0819.recipes.*
+import org.bukkit.plugin.java.JavaPlugin
 
+
+class Main : JavaPlugin() {
+    companion object {
+        @JvmStatic
+        val recipeList = ArrayList<UHCRecipe>()
+    }
+
+    override fun onEnable() {
+        server.pluginManager.registerEvents(Event(this), this)
+        recipe()
+        server.logger.info("Recipe is enabled")
+    }
+
+    private fun recipe() {
+        recipeList.addAll(arrayOf(
+            ApprenticeBow().register(),
+            ApprenticeHelmet().register(),
+            ApprenticeSword().register(),
+            ArrowEconomy().register(),
+            ArtemisBook().register(),
+            BookOfPower().register(),
+            BookOfProtection().register(),
+            BookOfSharpening().register(),
+            BrewingArtifact().register(),
+            DragonArmor().register(),
+            DragonSword().register(),
+            EveIsTemptation().register(),
+            GoldenHead().register(),
+            GoldPack().register(),
+            IronEconomy().register(),
+            LeatherEconomy().register(),
+            LightAnvil().register(),
+            MasterCompass().register(),
+            NetherArtifact().register(),
+            NewApple().register(),
+            NotchApple().register(),
+            PhilosopherPickaxe().register(),
+            QuickPick().register(),
+            Saddle().register(),
+            SparklingWatermelon().register(),
+            SugarRush().register(),
+            Tarnhelm().register()
+        ))
+//        val leatherHelmet = ItemStack(Material.LEATHER_HELMET).apply {
+//            itemMeta = itemMeta.apply {
+//                (this as LeatherArmorMeta).setColor(Color.PURPLE)
+//            }
+//        }
+    }
+}
