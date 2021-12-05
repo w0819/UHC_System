b4ec1f48276710396554f172aa52602d960e8b71
w0819 <hhha3730@gmail.com>
2021-12-05 12:12:20 +0900

    와 1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,11 @@ java {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
-    implementation("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    implementation("io.papermc.paper:paper-api:1.18-R0.1-SNAPSHOT")
     implementation("io.github.monun:tap-api:4.1.10")
     implementation("io.github.monun:kommand-api:2.6.6")
     implementation("net.projecttl:InventoryGUI-api:4.1.8")
-    implementation("io.github.monun:heartbeat-coroutines:0.0.1")
+    implementation("io.github.monun:heartbeat-coroutines:0.0.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
 }
 
